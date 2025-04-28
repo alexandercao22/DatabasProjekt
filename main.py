56310892afb1ef98abca7e57332f9dfa80c03718
HumminG <salliban03@gmail.com>
2025-04-28 10:00:45 +0200

    Created Database class

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""main"""
 import mysql.connector
 
 print("C++ > Python")
+
+class Database:
+    """database"""
+    def __init__(self, host, user, password, database) :
+        self.connection = mysql.connector.connect(
+            host = host,
+            user = user,
+            password = password,
+            database = database
+        )
+        self.cursor = self.connection.cursor(dictionary=True)
+
+db = Database('localhost', 'root', '1234', 'vsp')
