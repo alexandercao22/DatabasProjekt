379dedee12639e263bf0cb89d38377a5311660b9
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-04-29 10:55:59 +0200

    Input class

---
+++ Input.py
@@ -0,0 +1,86 @@
+"""I/O"""
+# from Database import Database
+
+class Input:
+    def GetInput(self):
+        return input("-> ")
+    
+    def GetChannelOrVideo(self, num):
+        run = True
+        while (run):
+            if (num == "return"):
+                return 0
+
+            try:
+                num = int(num)
+            except Exception:
+                print(f"Error: {num} is not a valid command! Try again:")
+                num = self.GetInput("-> ")
+                continue
+            run = False
+        return num
+    
+    def GetCommand(self, userInput):
+        if (userInput == "menu"):
+            print(f"Menu lol\nGeneral commands\n")
+            # print commands
+
+        elif (userInput == "search"):
+            print("Search for a channel or video:")
+            searchString = self.GetInput("-> ")
+            print(f"Searching for {searchString}\n")
+            # cursor.callproc("search", searchString)
+            # results = db.Search(searchString)
+            # display.PrintSearch(results)
+            
+            print("Enter a number to enter a channel or video:")
+            num = self.GetInput()
+            num = self.GetChannelOrVideo(num)
+
+            if (num == 0):
+                return True
+            else:
+                # Get channel or video and enter it
+                print(f"Channel or video: {num}") # Remove this print after implemented
+
+        elif (userInput == "search sorted"):
+            print("Search for a channel or video:")
+            searchString = self.GetInput("-> ")
+            print(f"Searching for {searchString}\n")
+            # cursor.callproc("searchSorted", searchString)
+            # results = db.SearchSorted(searchString)
+            # display.PrintSearch(results)
+            
+            print("Enter a number to enter a channel or video:")
+            num = self.GetInput()
+            num = self.GetChannelOrVideo(num)
+
+        elif (userInput == "list channels"):
+            print(f"Some channels lol\n")
+            # results = db.ListChannels()
+            # display.PrintChannels(results)
+
+            print("Enter a number to enter a channel:")
+            num = self.GetInput()
+            num = self.GetChannelOrVideo(num)
+
+            # Get channel and enter it
+            print(f"Channel: {num}") # Remove this print after implemented
+
+        elif (userInput == "list videos"):
+            print(f"Some videos lol\n")
+            # results = db.ListVideos()
+            # display.PrintVideos(results)
+
+            print("Enter a number to enter a video:")
+            num = self.GetInput()
+            num = self.GetChannelOrVideo(num)
+
+            # Get video and enter it
+            print(f"Video: {num}") # Remove this print after implemented
+
+        elif (userInput == "quit"):
+            return False
+        else:
+            print(f"Error: {userInput} is not a valid command!")
+        return True
--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 """main"""
-from Database import Database
+import mysql.connector
+from Input import Input
 
-db = Database('localhost', 'root', '1234', 'vsp')
-channels = db.fetch_all('Videos') 
-for channel in channels:
-    print(channel['video_name'], channel['length'])
+class Database:
+    """database"""
+    def __init__(self, host, user, password, database) :
+        self.connection = mysql.connector.connect(
+            host = host,
+            user = user,
+            password = password,
+            database = database
+        )
+        self.cursor = self.connection.cursor(dictionary=True)
+
+    def fetch_all(self, table):
+        sql = f"SELECT * FROM {table}"
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
+
+if __name__ == "__main__":
+    db = Database('localhost', 'root', 'admin', 'vsp')
+    channels = db.fetch_all('Channels')
+    for channel in channels:
+        print(channel['channel_name'], channel['country'])
+    print("\n")
+
+    print("Welcome to VSP!\n\n" \
+    "--- Instructions ---\n" \
+    "command. descriptioon <- type \"command\" to execute description\n")
+
+    app = Input()
+    app.GetCommand("menu")
+
+    run = True
+    while (run):
+        command = app.GetInput()
+        run = app.GetCommand(command)
